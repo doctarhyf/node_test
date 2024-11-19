4f375afc93396aba95fea372aa1d198fab45dabc
Docta Rhyf <drrhyf@gmail.com>
2024-11-19 13:10:20 +0200

    dbg

--- index.js
+++ index.js
@@ -2,14 +2,66 @@ import { drawChineseEnglishTextLine } from "./prints_.mjs";
 import chart from "./chart.mjs";
 import doc, { fontsize, marg } from "./pdf.mjs";
 
-function drawAgentCard(doc, agent) {}
+function drawAgentStatCard(doc, i, agent, x, y) {
+  if (agent.chef_deq === "OUI") agent.poste = "DEQ";
+  if (agent.is_exp === "OUI") agent.poste = "EXP";
+
+  let { nom, postnom, prenom, poste, mingzi, matricule, phone, chef_deq } =
+    agent;
+
+  const agentdata = [
+    { lat: `${i + 1}. -> ` },
+    { lat: `${poste}: ` },
+    { zh: mingzi },
+    {
+      lat: ` - ${matricule}  ${nom} ${postnom} ${prenom} `,
+    },
+  ];
+
+  // finalchart.push(agentdata);
+  let {
+    x: xr,
+    y: yr,
+    w: wr,
+    h: hr,
+  } = drawChineseEnglishTextLine(doc, x, y, fontsize, agentdata);
+
+  if (poste === "SUP") {
+    doc.setFillColor(180, 0, 0);
+  } else if (poste === "DEQ") {
+    doc.setFillColor(0, 0, 180);
+  } else if (poste === "OPE") {
+    doc.setFillColor(0, 180, 0);
+  } else if (poste === "CHARG") {
+    doc.setFillColor(0, 180, 180);
+  } else if (poste === "NET") {
+    doc.setFillColor(180, 0, 180);
+  } else if (poste === "EXP") {
+    doc.setFillColor(90, 90, 170);
+  } else if (poste === "DEQ") {
+    doc.setFillColor(0, 0, 255);
+  } else {
+    doc.setFillColor(0, 0, 0);
+  }
+
+  doc.rect(
+    xr - fontsize / 2,
+    yr - fontsize / 2,
+    wr + fontsize,
+    hr + fontsize / 2,
+    "F"
+  );
+  drawChineseEnglishTextLine(doc, x, y, fontsize, agentdata);
+  y = yr + fontsize;
+
+  return y;
+}
 
 function printChart(chart) {
   if (!chart) {
     throw new Error("Cant print undefined or empty Chart");
   }
   let y = marg * 2;
-  let finalchart = [];
 
   const flatchart = chart.flat();
 
@@ -21,56 +73,7 @@ function printChart(chart) {
   doc.setTextColor("white");
 
   flatchart.map((agent, i) => {
-    if (agent.chef_deq === "OUI") agent.poste = "DEQ";
-    if (agent.is_exp === "OUI") agent.poste = "EXP";
-
-    let { nom, postnom, prenom, poste, mingzi, matricule, phone, chef_deq } =
-      agent;
-
-    const agentdata = [
-      { lat: `${i + 1}. -> ` },
-      { lat: `${poste}: ` },
-      { zh: mingzi },
-      {
-        lat: ` - ${matricule}  ${nom} ${postnom} ${prenom} `,
-      },
-    ];
-
-    finalchart.push(agentdata);
-    let {
-      x: xr,
-      y: yr,
-      w: wr,
-      h: hr,
-    } = drawChineseEnglishTextLine(doc, marg, y, fontsize, agentdata);
-
-    if (poste === "SUP") {
-      doc.setFillColor(180, 0, 0);
-    } else if (poste === "DEQ") {
-      doc.setFillColor(0, 0, 180);
-    } else if (poste === "OPE") {
-      doc.setFillColor(0, 180, 0);
-    } else if (poste === "CHARG") {
-      doc.setFillColor(0, 180, 180);
-    } else if (poste === "NET") {
-      doc.setFillColor(180, 0, 180);
-    } else if (poste === "EXP") {
-      doc.setFillColor(90, 90, 170);
-    } else if (poste === "DEQ") {
-      doc.setFillColor(0, 0, 255);
-    } else {
-      doc.setFillColor(0, 0, 0);
-    }
-
-    doc.rect(
-      xr - fontsize / 2,
-      yr - fontsize / 2,
-      wr + fontsize,
-      hr + fontsize / 2,
-      "F"
-    );
-    drawChineseEnglishTextLine(doc, marg, y, fontsize, agentdata);
-    y = yr + fontsize;
+    y = drawAgentStatCard(doc, i, agent, marg, y);
   });
 }
 
